53a23bcf963c0f9fd82b01a15f29d6de95e54276
김한결 <kmr5326@naver.com>
2023-10-26 14:32:14 +0900

    feat: 회원가입, 로그인 ,회원정보 수정 기능 구현

--- backend/member/src/main/java/com/booking/member/members/Member.java
+++ backend/member/src/main/java/com/booking/member/members/Member.java
@@ -18,24 +18,34 @@ public class Member {
     @Column(name = "members_id")
     private Integer id;
 
+    private String loginId;
+
+    @Setter
     private String email;
 
+    @Setter
     private Integer age;
 
+    @Setter
     private Gender gender;
 
+    @Setter
     @Column(length = 255)
     private String nickname;
 
+    @Setter
     @Column(length = 255)
     private String fullName;
 
     //@Column(columnDefinition = "TEXT")
+    @Setter
     @Column(length = 255)
     private String address;
 
+    @Setter
     private UserRole role;
 
+    @Setter
     private String profileImage;
 
     @OneToMany(mappedBy = "following")
---
+++ backend/member/src/main/java/com/booking/member/members/controller/MemberController.java
@@ -0,0 +1,69 @@
+package com.booking.member.members.controller;
+
+import com.booking.member.members.dto.ModifyRequestDto;
+import com.booking.member.members.dto.SignUpRequestDto;
+import com.booking.member.members.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/members")
+@Slf4j
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/signup")
+    Mono<ResponseEntity<String>> signup(@RequestBody SignUpRequestDto req) {
+        log.info("회원 가입 요청={}", req);
+        memberService.signup(req);
+        return Mono.just(ResponseEntity.ok().body("signup success"))
+                .onErrorResume(e -> Mono.just(ResponseEntity.badRequest().body(e.getMessage())));
+    }
+
+//    @GetMapping("/memberInfo/{loginId}")
+//    Mono<ResponseEntity<MemberInfoResponseDto>> loadMember(@PathVariable String loginId){
+//        log.info("유저 정보 조회 loginId={}",loginId);
+//        try{
+//            MemberInfoResponseDto memberInfo=memberService.loadMemberInfo(loginId);
+//            return Mono.just(ResponseEntity.ok().body(memberInfo));
+//        }
+//        catch (Exception e){
+////            return Mono.just(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+//            return Mono.;
+//        }
+////        return loginIdMono.flatMap(loginId -> {
+////            log.info("유저 정보 조회 loginId={}",loginId);
+////            MemberInfoResponseDto memberInfo=memberService.loadMemberInfo(loginId);
+////            return Mono.just(ResponseEntity.ok().body(memberInfo));
+////        }).onErrorResume(e -> {
+////            log.error("유저 정보 조회 중 에러 발생", e);
+//////            return Mono.error(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+////            return Mono.error(e);
+////        });
+//    }
+
+    @GetMapping("/afterLogin/{token}")
+    Mono<ResponseEntity<String>> loadMemberAfterLogin(@PathVariable String token) {
+        try {
+            return Mono.just(ResponseEntity.ok().body(token));
+        } catch (Exception e) {
+//            return Mono.just(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+            return Mono.error(e);
+        }
+    }
+
+    @PatchMapping("/modification")
+    Mono<ResponseEntity<String>> modifyMemberInfo(@RequestBody ModifyRequestDto req) {
+        log.info("회원 정보 수정 요청={}", req);
+        memberService.modifyMemberInfo(req);
+        return Mono.just(ResponseEntity.ok().body("유저 정보 수정 완료"))
+                .onErrorResume(e -> Mono.just(ResponseEntity.badRequest().body(e.getMessage())));
+
+    }
+}
