f2c753296b17db330ee317c9f8d1d48e2d4f4e68
신상원 <dnjsml30@naver.com>
2023-10-26 15:58:03 +0900

    feat: add jwt util

---
+++ backend/booking/src/main/java/com/booking/booking/util/jwt/JwtUtil.java
@@ -0,0 +1,29 @@
+package com.booking.booking.util.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import org.springframework.beans.factory.annotation.Value;
+
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private static String secret;
+    public static String getLoginEmailByToken(String token) {
+
+        byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
+
+        var key = Keys.hmacShaKeyFor(keyBytes);
+
+        Jws<Claims> claims = Jwts.parserBuilder()
+                                 .setSigningKey(key) // 여기에 서명 확인을 위한 키 설정
+                                 .build()
+                                 .parseClaimsJws(token);
+
+        // Subject 가져오기
+        return claims.getBody().getSubject();
+    }
+
+}
