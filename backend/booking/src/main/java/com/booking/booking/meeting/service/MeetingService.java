28d8fc05a5d2795e449d499609301914d1faa98c
신상원 <dnjsml30@naver.com>
2023-10-26 17:10:35 +0900

    feat: get userinfo by member-service in getMemberinfo

--- backend/booking/src/main/java/com/booking/booking/meeting/service/MeetingService.java
+++ backend/booking/src/main/java/com/booking/booking/meeting/service/MeetingService.java
@@ -18,25 +18,46 @@ public class MeetingService {
     private final MeetingRepository meetingRepository;
     private static final String GATEWAY_URL = "http://localhost:8999";
 
-    public Mono<Void> arrangeMeeting(String userEmail) {
-
-        return getMemberInfoByEmail(userEmail)
+    public void arrangeMeeting(String userEmail) {
+        log.info("start arrageMettings");
+        getMemberInfoByEmail(userEmail)
             .flatMap(memberInfo -> {
-                // 비즈니스 로직
+                log.info(memberInfo.toString());
+                log.info("hello world!!!");
                 return Mono.empty();
-            });
+            })
+            .subscribe( // 이 부분이 추가되어야 합니다.
+                result -> {
+                    // onNext 이벤트 처리
+                    // 데이터 처리가 성공적으로 완료되면 이 부분이 호출됩니다.
+                },
+                error -> {
+                    // onError 이벤트 처리
+                    // 에러가 발생하면 이 부분이 호출됩니다.
+                    log.error("Error occurred: ", error);
+                },
+                () -> {
+                    // onComplete 이벤트 처리
+                    // 스트림이 성공적으로 완료되면 이 부분이 호출됩니다.
+                    log.info("Completed arrangeMeetings");
+                }
+            );
+        log.info("hello"); // 이 로그 메시지는 구독이 시작되자마자 출력될 것입니다.
     }
 
     private Mono<MemberInfoResponse> getMemberInfoByEmail(String userEmail) {
-
+        log.info("start inter-server comm");
         WebClient webClient = WebClient.builder().build();
         URI uri = URI.create(GATEWAY_URL + "/api/members/memberInfo/" + userEmail);
 
-        return webClient.get()
-                        .uri(uri)
-                        .retrieve()
-                        .onStatus(HttpStatus::is4xxClientError, response -> Mono.error(RuntimeException::new))
-                        .onStatus(HttpStatus::is5xxServerError, response -> Mono.error(RuntimeException::new))
-                        .bodyToMono(MemberInfoResponse.class);
+        Mono<MemberInfoResponse> memberInfoResponse = webClient.get()
+                                                               .uri(uri)
+                                                               .retrieve()
+                                                               .onStatus(HttpStatus::is4xxClientError, response -> Mono.error(RuntimeException::new))
+                                                               .onStatus(HttpStatus::is5xxServerError, response -> Mono.error(RuntimeException::new))
+                                                               .bodyToMono(MemberInfoResponse.class);
+
+        log.info("end inter-server comm");
+        return memberInfoResponse;
     }
 }
--- backend/booking/src/main/java/com/booking/booking/util/jwt/JwtUtil.java
+++ backend/booking/src/main/java/com/booking/booking/util/jwt/JwtUtil.java
@@ -4,26 +4,36 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Slf4j
+@Component
 public class JwtUtil {
 
-    @Value("${jwt.secret}")
     private static String secret;
-    public static String getLoginEmailByToken(String token) {
 
+    @Value("${jwt.secret}")
+    public void setSecret(String secret) {
+        JwtUtil.secret = secret;
+    }
+    public static String getLoginEmailByToken(String token) {
         //bearer
         token = token.substring(7);
 
-        byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] keyBytes = decoder.decode(secret);
 
         var key = Keys.hmacShaKeyFor(keyBytes);
 
+            // 파싱 과정에서 catch
         Jws<Claims> claims = Jwts.parserBuilder()
-                                 .setSigningKey(key) // 여기에 서명 확인을 위한 키 설정
-                                 .build()
-                                 .parseClaimsJws(token);
+                                     .setSigningKey(key) // 여기에 서명 확인을 위한 키 설정
+                                     .build()
+                                     .parseClaimsJws(token);
+
 
         // Subject 가져오기
         return claims.getBody().getSubject();
