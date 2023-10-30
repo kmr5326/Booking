ed3a01ccdcdf5c291fe4f0029bbe7c5a53e5754f
김한결 <kmr5326@naver.com>
2023-10-30 11:39:02 +0900

    test: 유저 정보 조회 기능 테스트중

--- backend/member/src/test/java/com/booking/member/members/controller/MemberControllerTest.java
+++ backend/member/src/test/java/com/booking/member/members/controller/MemberControllerTest.java
@@ -1,21 +1,24 @@
 package com.booking.member.members.controller;
 
-import static org.mockito.Mockito.doNothing;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import com.booking.member.members.dto.MemberInfoResponseDto;
 import com.booking.member.members.dto.SignUpRequestDto;
 import com.booking.member.util.ControllerTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class MemberControllerTest extends ControllerTest {
 
@@ -37,7 +40,7 @@ public class MemberControllerTest extends ControllerTest {
                     preprocessRequest(prettyPrint()), // json을 이쁘게 표시해라
                     preprocessResponse(prettyPrint()),
                     requestFields(
-                        fieldWithPath("loginId").type(JsonFieldType.STRING).description("닉네임"),
+                        fieldWithPath("loginId").type(JsonFieldType.STRING).description("로그인id"),
                         fieldWithPath("email").description("이메일"),
                         fieldWithPath("age").description("나이"),
                         fieldWithPath("gender").description("성별, 남자 : MALE, 여자 : FEMALE"),
@@ -46,6 +49,38 @@ public class MemberControllerTest extends ControllerTest {
                         fieldWithPath("address").description("주소")
                     ))
             );
+    }
+
+    @Test
+    @DisplayName("회원 정보를 조회한다.")
+    void t2() throws Exception{
+        String loginId= "1234";
+        MemberInfoResponseDto responseDto=new MemberInfoResponseDto("1234","email",10,"MALE","mono","monono","addr","img","google");
+
+        when(memberService.loadMemberInfo(any())).thenReturn(responseDto);
+
+        mockMvc.perform(get(baseUrl + "/memberInfo/{loginId}", loginId)
+                        .contentType(MediaType.APPLICATION_JSON) // JSON
+        ).andExpect(status().isOk()) // 200 반환
+                .andDo(
+                        document("/members/info",
+                                preprocessResponse(prettyPrint()),
+                                pathParameters(
+                                        parameterWithName("loginId").description("로그인 id")
+                                ),
+                                responseFields(
+                                        fieldWithPath("loginId").description("로그인 id"),
+                                        fieldWithPath("email").description("이메일"),
+                                        fieldWithPath("age").description("나이"),
+                                        fieldWithPath("gender").description("성별"),
+                                        fieldWithPath("nickname").description("닉네임"),
+                                        fieldWithPath("fullname").description("이름"),
+                                        fieldWithPath("address").description("주소"),
+                                        fieldWithPath("profileImage").description("프로필 이미지"),
+                                        fieldWithPath("provider").description("google,kakao")
+                                ))
+
+                );
     }
 
 }
