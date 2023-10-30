739a676b00fbefed95c9fe8ee4e669bc4c7e9c0e
신상원 <dnjsml30@naver.com>
2023-10-30 17:15:17 +0900

    feat: messagemapping message send 구현

--- backend/chat/src/main/java/com/booking/chat/chat/controller/ChatController.java
+++ backend/chat/src/main/java/com/booking/chat/chat/controller/ChatController.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
 
     private final KafkaTemplate<String, KafkaMessage> kafkaTemplate;
+
+    // 클라이언트에서 /publish/message 로 메시지를 전송
+    @MessageMapping("/message")
+    public void sendMessage(@Payload KafkaMessage message, @Payload String chatroomId) {
+        kafkaTemplate.send("Chatroom_" + chatroomId, message); // Kafka로 메세지 전달
+    }
 
     @PostMapping("/{chatRoomId}")
     public ResponseEntity<String> sendMessage(@PathVariable String chatRoomId, @RequestBody String message) {
