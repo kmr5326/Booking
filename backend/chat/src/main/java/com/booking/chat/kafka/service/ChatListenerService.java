3d786afca5b42e6c0027d9a981d79f89a362d7a3
신상원 <dnjsml30@naver.com>
2023-10-30 17:20:13 +0900

    feat: kafka 통신 구현

---
+++ backend/chat/src/main/java/com/booking/chat/kafka/service/ChatListenerService.java
@@ -0,0 +1,21 @@
+package com.booking.chat.kafka.service;
+
+import com.booking.chat.kafka.domain.KafkaMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class ChatListenerService {
+
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    @KafkaListener(topics = "Chatroom_*", groupId = "chat", containerFactory = "kafkaListenerContainerFactory")
+    public void listenGroupChat(KafkaMessage message, String chatroomId) {
+        simpMessagingTemplate.convertAndSend("/subscribe/" + chatroomId, message); // WebSocket을 통해 클라이언트에게 메세지 전송
+    }
+}
