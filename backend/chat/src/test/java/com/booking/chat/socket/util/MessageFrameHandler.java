a11c106338e2017ca05c760ff0e7488a027640cf
신상원 <dnjsml30@naver.com>
2023-10-31 13:59:46 +0900

    test: add test messageHandler

---
+++ backend/chat/src/test/java/com/booking/chat/socket/util/MessageFrameHandler.java
@@ -0,0 +1,29 @@
+package com.booking.chat.socket.util;
+
+import java.lang.reflect.Type;
+import java.util.concurrent.CompletableFuture;
+import lombok.Getter;
+import org.springframework.messaging.simp.stomp.StompFrameHandler;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+
+public class MessageFrameHandler<T> implements StompFrameHandler {
+
+    @Getter
+    private final CompletableFuture<T> completableFuture = new CompletableFuture<>();
+
+    private final Class<T> tClass;
+
+    public MessageFrameHandler(Class<T> tClass) {
+        this.tClass = tClass;
+    }
+
+    @Override
+    public Type getPayloadType(StompHeaders headers) {
+        return this.tClass;
+    }
+
+    @Override
+    public void handleFrame(StompHeaders headers, Object payload) {
+        completableFuture.complete((T)payload);
+    }
+}
