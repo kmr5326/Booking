39a24a3809bff89cb9df64fe2a48991990afdcad
신상원 <dnjsml30@naver.com>
2023-10-31 14:32:19 +0900

    feat: add date serialize

--- backend/chat/src/main/java/com/booking/chat/kafka/domain/KafkaMessage.java
+++ backend/chat/src/main/java/com/booking/chat/kafka/domain/KafkaMessage.java
@@ -1,5 +1,9 @@
 package com.booking.chat.kafka.domain;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -18,5 +22,8 @@ public class KafkaMessage implements Serializable {
 
     private String message;
     private String sender;
+
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDateTime sendTime;
 }
