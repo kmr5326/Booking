b70647cff2eb09a909859a4f493a73492a332bf8
신상원 <dnjsml30@naver.com>
2023-10-31 12:04:30 +0900

    feat: settings for mongo auto increment

--- backend/chat/src/main/java/com/booking/chat/chat/domain/Message.java
+++ backend/chat/src/main/java/com/booking/chat/chat/domain/Message.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
@@ -17,10 +18,18 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "chats")
 public class Message {
 
-    @Id private Long messageId;
+    @Transient
+    public static final String SEQUENCE_NAME = "message_sequence";
+
+    @Id
+    private Long _id;
+
     private Long chatroomId;
+
     private Long memberId;
+
     private String content;
+
     @CreatedDate
     private Date timestamp;
 
