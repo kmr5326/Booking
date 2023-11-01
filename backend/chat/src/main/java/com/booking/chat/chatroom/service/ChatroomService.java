374383ee074953e5d1d1cce8b920f1b6ceac9808
신상원 <dnjsml30@naver.com>
2023-11-01 23:42:15 +0900

    feat: create exception handler for already subscribed chatroom

--- backend/chat/src/main/java/com/booking/chat/chatroom/service/ChatroomService.java
+++ backend/chat/src/main/java/com/booking/chat/chatroom/service/ChatroomService.java
@@ -7,6 +7,7 @@ import com.booking.chat.chatroom.dto.response.ChatroomListResponse;
 import com.booking.chat.chatroom.exception.ChatroomException;
 import com.booking.chat.chatroom.repository.ChatroomRepository;
 import com.booking.chat.global.exception.ErrorCode;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -30,6 +31,10 @@ public class ChatroomService {
         return chatroomRepository.findById(joinChatroomRequest.meetingId())
             .switchIfEmpty(Mono.error(new ChatroomException(ErrorCode.CHATROOM_NOT_FOUND)))
             .flatMap(chatroom -> {
+                List<Long> members = chatroom.getMemberList();
+                if(members.contains(joinChatroomRequest.memberId())) {
+                    return Mono.error(new ChatroomException(ErrorCode.MEMBER_ALREADY_EXISTS));
+                }
                 chatroom.getMemberList().add(joinChatroomRequest.memberId());
                 return chatroomRepository.save(chatroom);
             })
--- backend/chat/src/main/java/com/booking/chat/global/exception/ErrorCode.java
+++ backend/chat/src/main/java/com/booking/chat/global/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    CHATROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 채팅방입니다.");
+    CHATROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 채팅방입니다."),
+    MEMBER_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "이미 가입한 채팅방입니다.");
 
     private final HttpStatus httpStatus;
     private final String errorMessage;
