56f9334f37abfe6663a0871366738256548bb8c4
신상원 <dnjsml30@naver.com>
2023-11-02 15:43:23 +0900

    feat: create JwtUtil

---
+++ backend/book/src/main/java/com/booking/book/book/domain/book.java
@@ -0,0 +1,21 @@
+package com.booking.book.book.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Document
+public class book {
+
+    @Id String isbn;
+
+
+}
