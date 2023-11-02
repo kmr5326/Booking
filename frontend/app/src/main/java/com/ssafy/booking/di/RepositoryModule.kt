823dd3cdad09de5283396831afd6123da78a5855
김재만 <lovesay00@nate.com>
2023-11-02 17:22:39 +0900

    feafeat: 회원가입 페이지 구현

--- frontend/app/src/main/java/com/ssafy/booking/di/RepositoryModule.kt
+++ frontend/app/src/main/java/com/ssafy/booking/di/RepositoryModule.kt
@@ -2,11 +2,14 @@ package com.ssafy.booking.di
 
 import com.ssafy.data.remote.api.ChatListApi
 import com.ssafy.data.repository.ChatRepositoryImpl
+import com.ssafy.data.remote.api.MemberApi
 import com.ssafy.data.repository.GoogleRepositoryImpl
 import com.ssafy.data.repository.GoogleDataSourceImpl
+import com.ssafy.data.repository.MemberRepositoryImpl
 import com.ssafy.domain.repository.ChatRepository
 import com.ssafy.domain.repository.GoogleRepository
 import dagger.Binds
+import com.ssafy.domain.repository.MemberRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -26,6 +29,12 @@ class RepositoryModule {
         return GoogleRepositoryImpl(
             googleDataSourceImpl
         )
+    }
+
+    @Provides
+    @Singleton
+    fun provideMemberRepository(api: MemberApi): MemberRepository {
+        return MemberRepositoryImpl(api)
     }
 
     @Provides
