0f2e8dc3745eaab95bfc69887b1b36c4dcd12316
신상원 <dnjsml30@naver.com>
2023-11-03 17:23:39 +0900

    test: 책 조회 테스트

--- backend/book/src/test/java/com/booking/book/book/controller/BookControllerTest.java
+++ backend/book/src/test/java/com/booking/book/book/controller/BookControllerTest.java
@@ -1,16 +1,17 @@
 package com.booking.book.book.controller;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document;
 import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.documentationConfiguration;
-import static reactor.core.publisher.Mono.when;
 
 import com.booking.book.book.dto.response.BookResponse;
 import com.booking.book.util.ControllerTest;
+import java.time.LocalDate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -36,13 +37,13 @@ class BookControllerTest extends ControllerTest {
     @DisplayName("책 목록 검색 된다")
     @Test
     void searchBookListByTitleParameter() throws Exception {
-        BookResponse bookResponse = new BookResponse("이기적 유전자", "작가", "이미지", "장르", "내용");
-        Flux<BookResponse> mockResponse = Flux.just(bookResponse);
+        BookResponse bookResponse = new BookResponse("이기적 유전자", "작가", "이미지", "장르", LocalDate.now(),"내용");
+        BookResponse bookResponse2 = new BookResponse("이기적 유전자", "작가", "이미지", "장르", LocalDate.now(),"내용");
 
-        when(bookService.searchBookListByTitleAndRelevance(any(String.class))).thenReturn(mockResponse);
+        when(bookService.searchBookListByTitleAndRelevance(any())).thenReturn(Flux.just(bookResponse, bookResponse2));
 
         webTestClient.get()
-            .uri(BASE_URL)
+                     .uri(uriBuilder -> uriBuilder.path(BASE_URL).queryParam("title", "이기적 유전자").build())
             .attribute("title", "이기적 유전자")
             .exchange()
             .expectStatus()
@@ -51,11 +52,12 @@ class BookControllerTest extends ControllerTest {
             .consumeWith(document("book/list",
                 preprocessResponse(prettyPrint()),
                 responseFields(
-                    fieldWithPath("title").type(JsonFieldType.STRING).description("책 제목"),
-                    fieldWithPath("author").type(JsonFieldType.STRING).description("작가"),
-                    fieldWithPath("coverImage").type(JsonFieldType.STRING).description("책 커버 이미지"),
-                    fieldWithPath("genre").type(JsonFieldType.STRING).description("책 장르"),
-                    fieldWithPath("content").type(JsonFieldType.STRING).description("책 소개")
+                    fieldWithPath("[].title").type(JsonFieldType.STRING).description("책 제목"),
+                    fieldWithPath("[].author").type(JsonFieldType.STRING).description("작가"),
+                    fieldWithPath("[].coverImage").type(JsonFieldType.STRING).description("책 커버 이미지"),
+                    fieldWithPath("[].genre").type(JsonFieldType.STRING).description("책 장르"),
+                    fieldWithPath("[].publishDate").description("책 출판일"),
+                    fieldWithPath("[].content").type(JsonFieldType.STRING).description("책 소개")
                 ))
             );
     }
