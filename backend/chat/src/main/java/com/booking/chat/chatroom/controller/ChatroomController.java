1442480825bc5be824ab5b3ba062d14e17dfb28e
신상원 <dnjsml30@naver.com>
2023-11-06 16:55:45 +0900

    feat: test

--- backend/chat/src/main/java/com/booking/chat/chatroom/controller/ChatroomController.java
+++ backend/chat/src/main/java/com/booking/chat/chatroom/controller/ChatroomController.java
@@ -9,6 +9,7 @@ import com.booking.chat.global.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -66,7 +67,7 @@ public class ChatroomController {
     }
 
 
-    @GetMapping("/list")
+    @GetMapping(value = "/list", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<ChatroomListResponse> getChatroomListByMemberId(@RequestHeader(AUTHORIZATION) String token) {
         Long memberId = JwtUtil.getMemberIdByToken(token);
         log.info(" {} member request chatroomList ", memberId);
