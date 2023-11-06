aba13aae2beecc1c2f7beb5f680f9ceaa75b287a
서현영 <shy1624@naver.com>
2023-11-06 15:16:38 +0900

    feat: 위치 기반 모임 검색

--- backend/booking/src/main/java/com/booking/booking/meeting/repository/MeetingRepository.java
+++ backend/booking/src/main/java/com/booking/booking/meeting/repository/MeetingRepository.java
@@ -7,4 +7,19 @@ import java.util.Optional;
 
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
     Optional<Meeting> findById(Long id);
+
+//    SELECT *, ( 6371 * acos( cos( radians(-1.0) ) * cos( radians( `lat` ) ) * cos( radians( `lgt` ) - radians(-1.0) ) + sin( radians(-1.0) ) * sin( radians( `lat` ) ) ) ) AS distance
+//    FROM `meetings`
+//    GROUP BY meeting_id
+//    HAVING distance <= 10
+//    ORDER BY distance ASC
+
+//    @Query(value =
+//            "SELECT *, " +
+//            "( 6371 * acos( cos( radians(:lat) ) * cos( radians( lat ) ) * cos( radians( lgt ) - radians(:lgt) ) " +
+//            "+ sin( radians(:lat) ) * sin( radians( lat ) ) ) ) AS distance " +
+//            "FROM meetings GROUP BY meeting_id HAVING distance <= :radius ORDER BY distance ASC",
+//            nativeQuery = true)
+//    List<Meeting> findMeetingsWithinRadius
+//        (@Param("lat") double lat, @Param("lgt") double lgt, @Param("radius") double radius);
 }
