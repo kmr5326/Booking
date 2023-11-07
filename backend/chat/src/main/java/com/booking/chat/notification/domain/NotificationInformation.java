7d884510d17010e00e3b2c6d65eedef67ce0ae1e
신상원 <dnjsml30@naver.com>
2023-11-07 20:37:50 +0900

    feat: add notificationInformation domain

---
+++ backend/chat/src/main/java/com/booking/chat/notification/domain/NotificationInformation.java
@@ -0,0 +1,25 @@
+package com.booking.chat.notification.domain;
+
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Document
+public class NotificationInformation {
+
+    @Id
+    private Long _id;
+
+    private Long memberId;
+
+    private String deviceToken;
+}
