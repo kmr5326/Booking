8d2d7e27ad825f1fd6e625bc798cbb0b8941eb02
kmr5326 <kmr5326@naver.com>
2023-11-08 23:29:03 +0900

    feat: 회원 탈퇴 기능 수정

--- backend/book/src/test/java/com/booking/book/memberbook/controller/MemberBookControllerTest.java
+++ backend/book/src/test/java/com/booking/book/memberbook/controller/MemberBookControllerTest.java
@@ -2,15 +2,14 @@ package com.booking.book.memberbook.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document;
 import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.documentationConfiguration;
 
 import com.booking.book.book.dto.response.BookResponse;
 import com.booking.book.memberbook.domain.MemberBook;
+import com.booking.book.memberbook.domain.Note;
 import com.booking.book.memberbook.dto.request.MemberBookRegistRequest;
 import com.booking.book.util.ControllerTest;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,7 +22,9 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 class MemberBookControllerTest extends ControllerTest {
 
@@ -33,39 +34,37 @@ class MemberBookControllerTest extends ControllerTest {
     void setUp() {
         MockitoAnnotations.openMocks(this);
         this.webTestClient = WebTestClient.bindToController(new MemberBookController(memberBookService))
-                                          .configureClient()
-                                          .filter(
-                                              documentationConfiguration(this.restDocumentation))
-                                          .build();
+                .configureClient()
+                .filter(
+                        documentationConfiguration(this.restDocumentation))
+                .build();
     }
 
-//    @DisplayName("내 서재 책 등록")
-//    @Test
-//    void t1() throws Exception {
+    @DisplayName("내 서재 책 등록")
+    @Test
+    void t1() throws Exception {
 //        MemberBookRegistRequest request=new MemberBookRegistRequest("닉네임","isbn");
-////        MemberBook memberBook=new MemberBook("1","멤버 닉네임","책 isbn",new ArrayList<>())
-//
-//        when(memberBookService.registerMemberBook(any()).thenReturn(Mono.empty()));
-//
-//        webTestClient.get()
-//                .uri(uriBuilder -> uriBuilder.path(BASE_URL + "/searchByTitle").queryParam("title", "이기적 유전자").build())
-//                .attribute("title", "이기적 유전자")
-//                .exchange()
-//                .expectStatus()
-//                .isOk()
-//                .expectBody()
-//                .consumeWith(document("book/listByTitle",
-//                        preprocessResponse(prettyPrint()),
-//                        responseFields(
-//                                fieldWithPath("[].title").type(JsonFieldType.STRING).description("책 제목"),
-//                                fieldWithPath("[].author").type(JsonFieldType.STRING).description("작가"),
-//                                fieldWithPath("[].coverImage").type(JsonFieldType.STRING).description("책 커버 이미지"),
-//                                fieldWithPath("[].genre").type(JsonFieldType.STRING).description("책 장르"),
-//                                fieldWithPath("[].publishDate").description("책 출판일"),
-//                                fieldWithPath("[].content").type(JsonFieldType.STRING).description("책 소개"),
-//                                fieldWithPath("[].isbn").type(JsonFieldType.STRING).description("ISBN")
-//                        ))
-//                );
-//    }
+        List<Note> notes=new ArrayList<>();
+        notes.add(new Note("메모", LocalDateTime.now()));
+        MemberBook memberBook=new MemberBook("1","멤버 닉네임","책 isbn",notes,LocalDateTime.now());
+
+        when(memberBookService.registerMemberBook(any()).thenReturn(Mono.just(memberBook)));
+
+        webTestClient.post()
+                .uri(uriBuilder -> uriBuilder.path(BASE_URL + "/").build())
+                .header("Authorization","Bearer JWT")
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBody()
+                .consumeWith(document("memberBook/register",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("nickname").type(JsonFieldType.STRING).description("닉네임"),
+                                fieldWithPath("isbn").type(JsonFieldType.STRING).description("책 isbn")
+                        ))
+                );
+    }
 
 }
