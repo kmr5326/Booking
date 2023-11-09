a576a6a494c79872eeae0231b033fae14a83267e
kmr5326 <kmr5326@naver.com>
2023-11-09 23:03:35 +0900

    feat: 내서재 등록 책 삭제 기능

--- backend/book/src/main/java/com/booking/book/memberbook/controller/MemberBookController.java
+++ backend/book/src/main/java/com/booking/book/memberbook/controller/MemberBookController.java
@@ -53,8 +53,6 @@ public class MemberBookController {
                 });
     }
 
-    //TODO : 메모 추가하기
-
     @PostMapping("/note")
     public Mono<ResponseEntity<String>> registerNote(@RequestHeader(AUTHORIZATION) String token,
                                                 @RequestBody RegisterNoteRequest request) {
@@ -62,5 +60,17 @@ public class MemberBookController {
         return memberBookService.registerNote(request)
                 .flatMap(resp->Mono.just(ResponseEntity.ok().body(resp)))
                 .onErrorResume(e-> Mono.just(ResponseEntity.badRequest().body(e.getMessage())));
+    }
+
+    @DeleteMapping("/{memberBookId}")
+    public Mono<ResponseEntity<String>> deleteMemberBook(@RequestHeader(AUTHORIZATION) String token,
+                                                    @PathVariable String memberBookId) {
+        log.info("내 서재 책 삭제 요청 : {}",memberBookId);
+        return memberBookService.deleteMemberBook(memberBookId)
+                .flatMap(resp -> Mono.just(ResponseEntity.ok().body(resp)))
+                .onErrorResume(e->{
+                    log.error("내 서재 책 삭제 요청 에러 : {}",e.getMessage());
+                    return Mono.just(ResponseEntity.badRequest().body(e.getMessage()));
+                });
     }
 }
