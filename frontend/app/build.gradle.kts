ec456cb2740a6b1f7539341ba5e4939a7835f232
이지헌 <jiheon_114@naver.com>
2023-11-10 03:33:05 +0900

    feat:배포 버전 수정

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.ssafy.booking"
         minSdk = 28
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
