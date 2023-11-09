1c17b8fecb8c8049a6bbf33bdfcd67855da20af0
신상원 <dnjsml30@naver.com>
2023-11-09 09:43:08 +0900

    refactor: stomp handler disabled

--- backend/chat/src/main/java/com/booking/chat/chatroom/service/ChatroomService.java
+++ backend/chat/src/main/java/com/booking/chat/chatroom/service/ChatroomService.java
@@ -1,13 +1,16 @@
 package com.booking.chat.chatroom.service;
 
+import com.booking.chat.chat.domain.Message;
 import com.booking.chat.chatroom.domain.Chatroom;
 import com.booking.chat.chatroom.dto.request.ExitChatroomRequest;
 import com.booking.chat.chatroom.dto.request.InitChatroomRequest;
 import com.booking.chat.chatroom.dto.request.JoinChatroomRequest;
+import com.booking.chat.chatroom.dto.request.LastMessageRequest;
 import com.booking.chat.chatroom.dto.response.ChatroomListResponse;
 import com.booking.chat.chatroom.exception.ChatroomException;
 import com.booking.chat.chatroom.repository.ChatroomRepository;
 import com.booking.chat.global.exception.ErrorCode;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +20,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -81,6 +85,14 @@ public class ChatroomService {
                                  .map(Chatroom::getMeetingTitle);
     }
 
+    public Flux<Message> enterChatroom(Long chatroomId, Long memberId, LastMessageRequest lastMessageRequest) {
+
+        String chatroomKey = "chatroom-%d".formatted(chatroomId);
+        redisManager(chatroomKey, memberId).subscribe();
+
+        return Flux.empty();
+    }
+
     public Mono<Void> disconnectChatroom(Long chatroomId, Long memberId) {
 
         String chatroomName = "chatroom-%d".formatted(chatroomId);
@@ -98,5 +110,42 @@ public class ChatroomService {
                                     })
                                     .then()
             .onErrorResume(error -> Mono.error(new ChatroomException(ErrorCode.MEMBER_NOT_PART_OF_CHATROOM)));
+    }
+
+    private Mono<Void> redisManager(String chatroomKey, Long memberId) {
+        reactiveRedisTemplate.hasKey(chatroomKey)
+                             .publishOn(
+                                 Schedulers.boundedElastic()) // 블로킹 작업에 적합한 스레드 사용
+                             .flatMap(exists -> {
+                                 if (Boolean.FALSE.equals(exists)) {
+                                     return storeMemberStatusWithCreateKey(chatroomKey,
+                                         memberId);
+                                 } else {
+                                     return storeMemberStatus(chatroomKey, memberId);
+                                 }
+                             })
+                             .subscribe(
+                                 result -> {}, // onNext
+                                 error -> log.error("Error on storing member status", error) // onError
+                             );
+
+        return Mono.empty();
+    }
+
+    private Mono<Boolean> storeMemberStatusWithCreateKey(String chatroomKey, Long memberId) {
+        log.info(" {} member connected {} and stored by redis", memberId, chatroomKey);
+        List<Long> memberList = List.of(memberId);
+        return reactiveRedisTemplate.opsForValue()
+                                    .set(chatroomKey, memberList);
+    }
+
+    private Mono<Boolean> storeMemberStatus(String chatroomKey, Long memberId) {
+        log.info(" {} member connected {} by redis", memberId, chatroomKey);
+        return reactiveRedisTemplate.opsForValue()
+                                    .get(chatroomKey)
+                                    .defaultIfEmpty(new ArrayList<>())
+                                    .doOnNext(memberList -> memberList.add(memberId))
+                                    .flatMap(memberList -> reactiveRedisTemplate.opsForValue()
+                                                                                .set(chatroomKey, memberList));
     }
 }
