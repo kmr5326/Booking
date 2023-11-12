3e2f15222e317b63581784479f0cafa5263ae7ba
서현영 <shy1624@naver.com>
2023-11-12 23:04:17 +0900

    feat: 미팅 등록 시간 추가

--- backend/booking/src/main/resources/schema.sql
+++ backend/booking/src/main/resources/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS participants;
 DROP TABLE IF EXISTS waitlist;
+DROP TABLE IF EXISTS participants_state;
 DROP TABLE IF EXISTS meetinginfos;
 DROP TABLE IF EXISTS hashtag_meeting;
 DROP TABLE IF EXISTS meetings;
@@ -15,16 +16,9 @@ CREATE TABLE meetings (
                           meeting_title VARCHAR(255),
                           description VARCHAR(255),
                           max_participants INT,
-                          meeting_state VARCHAR(255)
-);
-
-CREATE TABLE meetinginfos (
-                              meetinginfo_id BIGINT PRIMARY KEY AUTO_INCREMENT,
-                              meeting_id BIGINT,
-                              date DATETIME,
-                              location VARCHAR(255),
-                              fee INT,
-                              FOREIGN KEY (meeting_id) REFERENCES meetings(meeting_id)
+                          meeting_state VARCHAR(255),
+                          created_at DATETIME,
+                          updated_at DATETIME
 );
 
 CREATE TABLE hashtags (
@@ -40,12 +34,19 @@ CREATE TABLE hashtag_meeting (
                                  FOREIGN KEY (hashtag_id) REFERENCES hashtags(hashtag_id)
 );
 
+CREATE TABLE meetinginfos (
+                              meetinginfo_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                              meeting_id BIGINT,
+                              date DATETIME,
+                              location VARCHAR(255),
+                              fee INT,
+                              FOREIGN KEY (meeting_id) REFERENCES meetings(meeting_id)
+);
+
 CREATE TABLE participants (
                               participant_id BIGINT PRIMARY KEY AUTO_INCREMENT,
                               member_id INT,
                               meeting_id BIGINT,
-                              attendance_status TINYINT(1),
-                              payment_status TINYINT(1),
                               FOREIGN KEY (meeting_id) REFERENCES meetings(meeting_id)
 );
 
@@ -56,11 +57,22 @@ CREATE TABLE waitlist (
                           FOREIGN KEY (meeting_id) REFERENCES meetings(meeting_id)
 );
 
+CREATE TABLE participants_state (
+                          state_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                          member_id INT,
+                          meetinginfo_id BIGINT,
+                          attendance_status TINYINT(1),
+                          payment_status TINYINT(1),
+                          FOREIGN KEY (meetinginfo_id) REFERENCES meetinginfos(meetinginfo_id)
+);
+
 CREATE TABLE posts (
                        post_id BIGINT PRIMARY KEY AUTO_INCREMENT,
                        meeting_id BIGINT,
                        member_id INT,
                        title VARCHAR(255),
                        content TEXT,
+                       created_at DATETIME,
+                       updated_at DATETIME,
                        FOREIGN KEY (meeting_id) REFERENCES meetings(meeting_id)
 );
