4a16d59c72875f3a99083080e8cf385c8e2c0820
이지헌 <jiheon_114@naver.com>
2023-11-14 01:26:40 +0900

    feat:ë내 좌표 불러와서 카카오 api를 통해 위경도변환 구현, 내 위치 수정하기 기능 구현

--- frontend/app/src/main/java/com/ssafy/booking/ui/location/SettingAddress.kt
+++ frontend/app/src/main/java/com/ssafy/booking/ui/location/SettingAddress.kt
@@ -9,8 +9,12 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.KeyboardArrowRight
+import androidx.compose.material.icons.outlined.LocationOn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -19,6 +23,7 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.core.app.ActivityCompat
@@ -36,6 +41,7 @@ import com.ssafy.booking.viewmodel.LocationViewModel
 import com.ssafy.booking.viewmodel.MyPageViewModel
 import com.ssafy.data.repository.token.TokenDataSource
 import com.ssafy.domain.model.SignInRequest
+import com.ssafy.domain.model.mypage.AddressnModifyRequest
 
 // 최상단 컴포저블
 @Composable
@@ -102,8 +108,10 @@ fun SetCurrentLocation() {
 
     val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
     val myPageViewModel : MyPageViewModel = hiltViewModel()
+    val locationViewModel : LocationViewModel = hiltViewModel()
+    val getAddressResponse by locationViewModel.getAddressResponse.observeAsState()
     val (myLocation, setMyLocation) = remember { mutableStateOf("0") }
-
+    val (addressName, setAddressName) = remember { mutableStateOf("") }
     // 주기적인 위치 업데이트를 위한 LocationRequest 객체 생성
     val locationRequest = LocationRequest.create().apply {
         interval = 10000 // 10초마다 위치 업데이트
@@ -147,13 +155,37 @@ fun SetCurrentLocation() {
                 }, 10000) // 10초 후에 위치 업데이트 중지
             }
         }
-
     }
-    Text(text=myLocation)
+
+    // MyLocaton에서 위도,경도 추출하는 함수.
+    fun extractLatitudeLongitude(location: String): Pair<Double, Double>? {
+        val regex = """Location\[fused (\d+\.\d+),(-?\d+\.\d+)""".toRegex()
+        val matchResult = regex.find(location)
+
+        return matchResult?.let {
+            val (latitude, longitude) = it.destructured
+            Pair(latitude.toDouble(), longitude.toDouble())
+        }
+    }
     LaunchedEffect(myLocation) {
+        // 리턴 @LaunchedEffect 하면 나머지 코드 블럭 실행 안 하고 종료.
+        val (lat, lgt) = extractLatitudeLongitude(myLocation) ?: return@LaunchedEffect
+
         Log.d("위치",myLocation)
+        Log.d("위치",lat.toString())
+        Log.d("위치",lgt.toString())
+
+        locationViewModel.getAddress(lgt.toString(), lat.toString())
+        // 이상한 곳에 위경도를 쏘면 주소가 안 뜨는데, 그럴 때는 주소를 불러오지 않음.
+        if (getAddressResponse?.body()?.documents!!.isNotEmpty()) {
+            setAddressName(getAddressResponse?.body()?.documents?.firstOrNull()?.address?.addressName!!)
+        }
     }
 
+    if (addressName != "") {
+        Text(text="현재 내 위치")
+        Text(text = addressName)
+    }
     Button(
         onClick = {
             requestPermissionsLauncher.launch(
@@ -162,34 +194,27 @@ fun SetCurrentLocation() {
                     Manifest.permission.ACCESS_COARSE_LOCATION
                 )
             )
-//            myPageViewModel.patchUserAddress(myLocation)
-
                   },
         colors = ButtonDefaults.buttonColors(
             containerColor = colorResource(id = R.color.booking_1),
             contentColor = colorResource(id = R.color.font_color)
         )
     ) {
-        Text(text = "위치 인증 체크")
+            Icon(Icons.Outlined.LocationOn, contentDescription = null, tint = Color(0xFF12BD7E))
+            Text(text = "현재 내 위치 불러오기")
+            Icon(Icons.Outlined.KeyboardArrowRight, contentDescription = null, tint = Color(0xFF12BD7E))
     }
 
     Button(
         onClick = {
             Log.d("위치",myLocation)
+            myPageViewModel.patchUserAddress(
+                AddressnModifyRequest(
+                    address = myLocation
+                ))
         },
         ) {
         Text(text="내 위치 수정하기")
 }
 
 }
-//
-//Row(
-//modifier = Modifier
-//.clickable {
-//    fetchLocation()
-//}
-//) {
-//    Icon(Icons.Outlined.LocationOn, contentDescription = null, tint = Color(0xFF12BD7E))
-//    Text(text = "현재 내 위치로 설정")
-//    Icon(Icons.Outlined.KeyboardArrowRight, contentDescription = null, tint = Color(0xFF12BD7E))
-//}
