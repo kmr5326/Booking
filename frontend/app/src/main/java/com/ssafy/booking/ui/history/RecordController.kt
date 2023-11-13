c3e18ceb09e842096c0b1a95cba3f653ebd782f8
PARKHEECHANG <shjc4623@gmail.com>
2023-11-13 23:38:07 +0900

    feat:RecordController FileUploader UI

--- frontend/app/src/main/java/com/ssafy/booking/ui/history/RecordController.kt
+++ frontend/app/src/main/java/com/ssafy/booking/ui/history/RecordController.kt
@@ -25,30 +25,37 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import android.Manifest
+import android.graphics.drawable.PaintDrawable
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.ssafy.booking.viewmodel.RecordViewModel
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ssafy.booking.viewmodel.PlayerViewModel
 import com.ssafy.booking.viewmodel.RecordingState
 import com.ssafy.booking.R
+import com.ssafy.booking.viewmodel.RecorderViewModel
 
 
 @Composable
 fun RecordController(
 ) {
     val context = LocalContext.current
-    val recordViewModel: RecordViewModel = hiltViewModel()
-
-    val recordingState by recordViewModel.recordingState.observeAsState(RecordingState.STOPPED)
+    val recorderViewModel: RecorderViewModel = hiltViewModel()
+    val recordingState by recorderViewModel.recordingState.observeAsState(RecordingState.STOPPED)
 
     // 권한 요청
     val requestPermissionLauncher = rememberLauncherForActivityResult(
@@ -56,74 +63,97 @@ fun RecordController(
         onResult = { isGranted: Boolean ->
             if (isGranted) {
                 Toast.makeText(context, "녹음을 시작합니다.", Toast.LENGTH_SHORT).show()
-                recordViewModel.startRecording()
+                recorderViewModel.startRecording()
             } else {
                 Toast.makeText(context, "녹음 기능을 사용하려면 권한을 수락해야 합니다.", Toast.LENGTH_SHORT).show()
             }
         }
     )
 
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(12.dp)
+    ) {
+        // 녹음 저장 버튼 - 오른쪽 상단 정렬
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(4.dp)
+        ) {
+            IconButton(
+                onClick = {
+                    if (recordingState == RecordingState.PAUSED) {
+                        recorderViewModel.stopRecording()
+                        Toast.makeText(context, "녹음을 저장합니다.", Toast.LENGTH_SHORT).show()
+                    }
+                },
+                modifier = Modifier.size(50.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.baseline_save_24),
+                    contentDescription = "녹음 저장",
+                    tint = if (recordingState == RecordingState.PAUSED) Color(0xFF101828) else Color.LightGray
+                )
+            }
+            Text(text = "저장하기", fontSize=12.sp)
+        }
 
-    // 시작
-    if (recordingState == RecordingState.STOPPED) {
-        IconButton(
-            onClick = {
-                requestPermissionLauncher.launch(Manifest.permission.RECORD_AUDIO)
-            },
+        // 나머지 컨트롤 - 중앙 정렬
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.align(Alignment.Center)
         ) {
-            Icon(
-                Icons.Filled.PlayArrow,
-                contentDescription = "녹음 시작",
-                modifier = Modifier
-                    .size(100.dp),
-                tint = Color(0xFF00C68E)
-            )
-        }
-    } else if (recordingState == RecordingState.STARTED || recordingState == RecordingState.RESUMED) {
-        IconButton(
-            onClick = {
-                recordViewModel.pauseRecording()
-                Toast.makeText(context, "녹음을 정지합니다.", Toast.LENGTH_SHORT).show()
-            },
-        ) {
-            Icon(
-                Icons.Filled.Menu,
-                contentDescription = "녹음 중지",
-                modifier = Modifier
-                    .size(100.dp),
-                tint = Color(0xFF00C68E)
-            )
-        }
-    } else if (recordingState == RecordingState.PAUSED) {
-        IconButton(
-            onClick = {
-                Log.d("RECORD_STATE", "$recordingState")
-                recordViewModel.resumeRecording()
-                Toast.makeText(context, "녹음을 재개합니다.", Toast.LENGTH_SHORT).show()
-                Log.d("RECORD_STATE", "$recordingState")
-            },
-        ) {
-            Icon(
-                Icons.Filled.PlayArrow,
-                contentDescription = "녹음 재개",
-                modifier = Modifier
-                    .size(100.dp),
-                tint = Color(0xFF00C68E)
-            )
-        }
-        IconButton(
-            onClick = {
-                recordViewModel.stopRecording()
-                Toast.makeText(context, "녹음을 저장합니다.", Toast.LENGTH_SHORT).show()
-            },
-        ) {
-            Icon(
-                Icons.Filled.ShoppingCart,
-                contentDescription = "녹음 저장",
-                modifier = Modifier
-                    .size(100.dp),
-                tint = Color(0xFF00C68E)
-            )
+            // 시작
+            if (recordingState == RecordingState.STOPPED) {
+                IconButton(
+                    onClick = {
+                        requestPermissionLauncher.launch(Manifest.permission.RECORD_AUDIO)
+                    },
+                    modifier = Modifier
+                        .size(100.dp),
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_play_arrow_24),
+                        contentDescription = "녹음 시작",
+                        tint = Color(0xFF00C68E)
+                    )
+                }
+            } else if (recordingState == RecordingState.STARTED || recordingState == RecordingState.RESUMED) {
+                IconButton(
+                    onClick = {
+                        recorderViewModel.pauseRecording()
+                        Toast.makeText(context, "녹음을 정지합니다.", Toast.LENGTH_SHORT).show()
+                    },
+                    modifier = Modifier
+                        .size(100.dp),
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_pause_circle_24),
+                        contentDescription = "녹음 중지",
+                        tint = Color(0xFFFF1658)
+                    )
+                }
+            } else if (recordingState == RecordingState.PAUSED) {
+                IconButton(
+                    onClick = {
+                        Log.d("RECORD_STATE", "$recordingState")
+                        recorderViewModel.resumeRecording()
+                        Toast.makeText(context, "녹음을 재개합니다.", Toast.LENGTH_SHORT).show()
+                        Log.d("RECORD_STATE", "$recordingState")
+                    },
+                    modifier = Modifier
+                        .size(100.dp),
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_play_arrow_24),
+                        contentDescription = "녹음 재개",
+                        tint = Color(0xFF00C68E)
+                    )
+                }
+            }
         }
     }
 }
