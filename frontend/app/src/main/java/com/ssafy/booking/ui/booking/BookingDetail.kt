9cded2fa98a02a5bf5dfe4c4bbf282a80e750e1a
이지헌 <jiheon_114@naver.com>
2023-11-14 21:47:18 +0900

    feat:북킹 디테일 페이지에서 역할,모임상태에 따른 분기 ê나누고 그에 따른 버튼 구현

--- frontend/app/src/main/java/com/ssafy/booking/ui/booking/BookingDetail.kt
+++ frontend/app/src/main/java/com/ssafy/booking/ui/booking/BookingDetail.kt
@@ -53,33 +53,47 @@ val tabTitles = listOf("모임 정보", "참가자", "게시판")
 fun BookingDetail(meetingId: Long) {
     val bookingViewModel: BookingViewModel = hiltViewModel()
     val getBookingDetailResponse by bookingViewModel.getBookingDetailResponse.observeAsState()
+    val getParticipantsResponse by bookingViewModel.getParticipantsResponse.observeAsState()
+    val getWaitingListResponse by bookingViewModel.getWaitingListResponse.observeAsState()
+
     val context = LocalContext.current
     val navController = LocalNavigation.current
     
     // 리더인지 아닌지
-    var isLeadered by remember { mutableStateOf(false) }
+    var memberRole by remember { mutableStateOf("GUEST") }
     // 모임 진행 상황
-    val status = getBookingDetailResponse?.body()?.meetingState
-    val snackbarHostState = remember { SnackbarHostState() }
-    LaunchedEffect(getBookingDetailResponse) {
-        val memberPk = App.prefs.getMemberPk()
-        val leaderId = getBookingDetailResponse?.body()?.leaderId
-        isLeadered = memberPk == leaderId?.toLong()
+    var meetingState by remember { mutableStateOf("PREPAIRING") }
+    LaunchedEffect(Unit) {
+        bookingViewModel.getBookingDetail(meetingId)
+        bookingViewModel.getParticipants(meetingId)
+        bookingViewModel.getWaitingList(meetingId)
     }
-    Scaffold(
-        bottomBar = {
-            if (status == "PREPARING") {
-                if (isLeadered) {
-                    PreparingBottomBarForLeader(navController)
-                } else {
-                    PreparingBottomBarForParticipant(meetingId, bookingViewModel, context)
-                }
-            }
-            if (status == "ONGOING") {
-                OngoingBottomBar(meetingId,bookingViewModel,context,isLeadered)
+    // 3개 중 하나라도 바뀌면 리렌더링
+    LaunchedEffect(getBookingDetailResponse, getParticipantsResponse, getWaitingListResponse) {
+        meetingState = getBookingDetailResponse?.body()?.meetingState ?: "PREPARING"
+        // 리더인지 확인
+        val memberPk = App.prefs.getMemberPk().toInt()
+        val leaderId = getBookingDetailResponse?.body()?.leaderId
+        if (memberPk == leaderId) {
+            memberRole = "LEADER"
+        } else {
+            // 참여자 명단 확인
+            val participants = getParticipantsResponse?.body()
+            val isParticipant = participants?.any { it.memberPk == memberPk }
 
+            // 대기자 명단 확인
+            val waitingList = getWaitingListResponse?.body()
+            val isWaiting = waitingList?.any { it.memberPk == memberPk }
+
+            when {
+                isParticipant == true -> memberRole = "PARTICIPANT" // 참여자
+                isWaiting == true -> memberRole = "WAITING" // 대기자
+                else -> memberRole = "GUEST" // 모두 해당되지 않으면 게스트
             }
         }
+    }
+
+    Scaffold(
     ) { paddingValues ->
         Column(modifier = Modifier.padding(paddingValues)) {
             BackTopBar(title = "모임 상세")
@@ -89,136 +103,267 @@ fun BookingDetail(meetingId: Long) {
                     when (index) {
                         0 -> BookingInfo(
                             meetingId = meetingId,
-                            isLeadered = isLeadered
+                            memberRole = memberRole,
+                            meetingState = meetingState,
                         )
                         1 -> BookingParticipants(
                             meetingId = meetingId,
-                            isLeadered = isLeadered,
-                            status = status!!
+                            memberRole = memberRole,
+                            meetingState = meetingState,
                         )
                         2 -> BookingBoard(
                             meetingId = meetingId,
-                            isLeadered = isLeadered
+                            memberRole = memberRole,
+                            meetingState = meetingState,
                         )
                     }
                 }
             )
+            BottomBar(memberRole,meetingState,meetingId,bookingViewModel,context,navController)
+
         }
     }
 }
 
+
 @Composable
-fun PreparingBottomBarForLeader(navController: NavController) {
-    Column(
+fun BottomBar(memberRole:String,meetingState:String,meetingId:Long,bookingViewModel: BookingViewModel,context:Context ,navController:NavController){
+    when (meetingState) {
+        "PREPARING" -> preparingBottomBar(memberRole,meetingId,bookingViewModel,context,navController)
+        "ONGOING" -> ongoingBottomBar(memberRole,meetingId,bookingViewModel,context,navController)
+        "FINISH" -> finishBottomBar(memberRole,meetingId)
+    }
+}
+
+// 상태가 PREPAIRING 일 때,
+@Composable
+fun preparingBottomBar(memberRole:String,meetingId:Long,bookingViewModel: BookingViewModel,context:Context ,navController:NavController) {
+    when (memberRole) {
+        "LEADER" -> {
+            ModifyButton(navController,meetingId,bookingViewModel,context)
+            StartButton(navController,meetingId,bookingViewModel,context)
+        }
+        "PARTICIPANT" ->
+            ExitButton(navController,meetingId,bookingViewModel,context)
+        "WAITING" ->
+            WaitingButton()
+        else -> {
+            // 게스트
+            JoinRequestButton(navController = navController,meetingId,bookingViewModel,context)
+        }
+    }
+}
+
+// 상태가 ONGOING 일 때,
+@Composable
+fun ongoingBottomBar(memberRole:String,meetingId:Long,bookingViewModel: BookingViewModel,context:Context ,navController:NavController){
+    when (memberRole) {
+        "LEADER" -> {
+            EndButton(navController,meetingId,bookingViewModel,context) // 종료
+            RestartButton(navController,meetingId,bookingViewModel,context) // 한 번 더
+            AttendCheckButton(navController,meetingId,bookingViewModel,context)
+            PayRequestButton(navController,meetingId,bookingViewModel,context)
+        }
+        "PARTICIPANT" -> {
+            AttendCheckButton(navController,meetingId,bookingViewModel,context)
+            PayRequestButton(navController,meetingId,bookingViewModel,context)
+        }
+        else -> {
+            // 게스트
+            AlreadyOngoingButton()
+        }
+    }
+}
+// 상태가 FINISH 일 때,
+@Composable
+fun finishBottomBar(memberRole:String,meetingId:Long){
+    AlreadyEndButton()
+}
+
+// 모임 가입 신청 버튼
+@Composable
+fun JoinRequestButton(navController: NavController,meetingId:Long,bookingViewModel: BookingViewModel,context: Context){
+    // 모임 참가신청
+    Button(onClick = {
+        val request = BookingJoinRequest(meetingId = meetingId)
+        bookingViewModel.postBookingJoin(meetingId, request)
+        val toastTop = Toast.makeText(context, "참가신청이 완료됐습니다.", Toast.LENGTH_LONG)
+        toastTop.setGravity(Gravity.TOP,0,0)
+        toastTop.show()
+    },
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .height(50.dp)
     ) {
-        Button(onClick = {
-            navController.navigate(AppNavItem.BookingSetTitle.route)
-        }) {
-            Text(text = "모임 수정")
-        }
-        Spacer(modifier = Modifier.height(8.dp))
-        Button(onClick = {
-            navController.navigate(AppNavItem.BookingSetLocation.route)
-        }) {
-            Text(text = "모임 확정")
-        }
+        Text("모임 참가 신청하기")
+    }
+
+}
+// 모임 확정 버튼
+@Composable
+
+fun StartButton(
+    navController: NavController,
+    meetingId: Long,
+    bookingViewModel: BookingViewModel,
+    context: Context
+){
+    Button(onClick = {
+        navController.navigate(AppNavItem.BookingSetLocation.route)
+    }) {
+        Text(text = "모임 확정")
+    }
+}
+// 모임 수정 버튼
+@Composable
+fun ModifyButton(
+    navController: NavController,
+    meetingId: Long,
+    bookingViewModel: BookingViewModel,
+    context: Context
+){
+    Button(onClick = {
+        navController.navigate(AppNavItem.BookingSetTitle.route)
+    }) {
+        Text(text = "모임 수정")
     }
 }
 
+// 모임 나가기 버튼
 @Composable
-fun PreparingBottomBarForParticipant(meetingId: Long, bookingViewModel: BookingViewModel, context: Context) {
-    Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-    horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        // 모임 참가신청
-        Button(onClick = {
-            val request = BookingJoinRequest(meetingId = meetingId)
-            bookingViewModel.postBookingJoin(meetingId, request)
-            val toastTop = Toast.makeText(context, "참가신청이 완료됐습니다.", Toast.LENGTH_LONG)
-            toastTop.setGravity(Gravity.TOP,0,0)
-            toastTop.show()
-        },
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp)
-        ) {
-            Text("모임 참가 신청하기")
-        }
-        
-        // 모임 나가기
-        Button(onClick = {
-            bookingViewModel.postBookingExit(meetingId)
-            val toastTop = Toast.makeText(context, "모임나가기.", Toast.LENGTH_LONG)
-            toastTop.setGravity(Gravity.TOP,0,0)
-            toastTop.show()
-        },
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp)
-        ) {
-            Text("모임 나가기")
-        }
-    }
-}
-@Composable
-fun OngoingBottomBar(meetingId:Long,bookingViewModel: BookingViewModel,context: Context,isLeadered:Boolean){
-    Column(
+fun ExitButton(
+    navController: NavController,
+    meetingId: Long,
+    bookingViewModel: BookingViewModel,
+    context: Context
+){
+    Button(onClick = {
+        bookingViewModel.postBookingExit(meetingId)
+        val toastTop = Toast.makeText(context, "모임나가기.", Toast.LENGTH_LONG)
+        toastTop.setGravity(Gravity.TOP,0,0)
+        toastTop.show()
+    },
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .height(50.dp)
     ) {
-        // 출석체크 버튼
-        Button(onClick = {
-            bookingViewModel.patchBookingAttend(meetingId)
+        Text("모임 나가기")
+    }
+}
+// 모임 출첵 버튼
+@Composable
+fun AttendCheckButton(
+    navController : NavController,
+    meetingId : Long,
+    bookingViewModel : BookingViewModel,
+    context : Context
+){
+    // 출석체크 버튼
+    Button(onClick = {
+        bookingViewModel.patchBookingAttend(meetingId)
 
-            val toastTop = Toast.makeText(context, "출석체크가 완료되었습니다.", Toast.LENGTH_LONG)
-            toastTop.setGravity(Gravity.TOP,0,0)
+        val toastTop = Toast.makeText(context, "출석체크가 완료되었습니다.", Toast.LENGTH_LONG)
+        toastTop.setGravity(Gravity.TOP,0,0)
+        toastTop.show()
+    },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+    ) {
+        Text("출석 체크하기")
+    }
+}
+// 모임 종료 버튼
+@Composable
+fun EndButton(
+    navController : NavController,
+    meetingId : Long,
+    bookingViewModel : BookingViewModel,
+    context : Context
+){
+    // 모임종료 버튼
+    Button(
+        onClick = {
+            bookingViewModel.patchBookingEnd(meetingId)
+            val toastTop = Toast.makeText(context, "모임이 종료되었습니다.", Toast.LENGTH_LONG)
+            toastTop.setGravity(Gravity.TOP, 0, 0)
             toastTop.show()
         },
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp)
-        ) {
-            Text("출석 체크하기")
-        }
-        // 방장일 때만 모임 종료, 한 번 더 중 택1
-        if (isLeadered) {
-            // 모임종료 버튼
-            Button(
-                onClick = {
-                    bookingViewModel.patchBookingEnd(meetingId)
-                    val toastTop = Toast.makeText(context, "모임이 종료되었습니다.", Toast.LENGTH_LONG)
-                    toastTop.setGravity(Gravity.TOP, 0, 0)
-                    toastTop.show()
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(50.dp)
-            ) {
-                Text("모임 종료하기")
-            }
-            // 한번 더 하기 버튼
-            Button(
-                onClick = {
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+    ) {
+        Text("모임 종료하기")
+    }
+
+}
+// 모임 다시 시작(한 번 더 ) 버튼
+@Composable
+fun RestartButton(
+    navController : NavController,
+    meetingId : Long,
+    bookingViewModel : BookingViewModel,
+    context : Context
+) {
+    // 한번 더 하기 버튼
+    Button(
+        onClick = {
 //                    bookingViewModel.patchBookingEnd(meetingId)
-                    val toastTop = Toast.makeText(context, "한 번 더하기(구현 중).", Toast.LENGTH_LONG)
-                    toastTop.setGravity(Gravity.TOP, 0, 0)
-                    toastTop.show()
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(50.dp)
-            ) {
-                Text("한번 더하기 (아직 구현 중)")
-            }
-        }
+            val toastTop = Toast.makeText(context, "한 번 더하기(구현 중).", Toast.LENGTH_LONG)
+            toastTop.setGravity(Gravity.TOP, 0, 0)
+            toastTop.show()
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+    ) {
+        Text("한번 더하기 (아직 구현 중)")
     }
+    
+}
+
+// 참가비 결제 버튼
+@Composable
+fun PayRequestButton(
+    navController : NavController,
+    meetingId : Long,
+    bookingViewModel : BookingViewModel,
+    context : Context
+){
+    Button(onClick = { /*TODO*/ }) {
+        Text(text="참가비 결제하기")
+    }
+}
+
+// 참가신청 승인 대기 버튼 ( 기다리는 중..)
+@Composable
+fun WaitingButton(
+) {
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = "가입 승인 대기 중..")
+    }
+}
+// 이미 참가한 사람에게 보여지는 버튼
+@Composable
+fun AlreadyJoinButton() {
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = "이미 참가한 모임입니다.")
+    }
+}
+
+// 이미 진행중인 모임에 대해 보여지는 버튼
+@Composable
+fun AlreadyOngoingButton() {
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = "이미 진행중인 모임입니다.")
+    }
+}
+
+// 이미 종료된 모임에 대해 보여지는 버튼
+@Composable
+fun AlreadyEndButton(){
+    Button(onClick = { /*TODO*/ }) {
+        Text(text = "이미 종료된 모임입니다.")
+    }
+
 }
