03056db1635d79c613e0e37a257d5b973c66a0da
신상원 <dnjsml30@naver.com>
2023-11-15 23:38:06 +0900

    refactor: refactor chatroomList

--- backend/chat/src/main/java/com/booking/chat/message/repository/MessageRepository.java
+++ backend/chat/src/main/java/com/booking/chat/message/repository/MessageRepository.java
@@ -1,6 +1,7 @@
 package com.booking.chat.message.repository;
 
 import com.booking.chat.message.domain.Message;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.data.mongodb.repository.Tailable;
@@ -13,5 +14,9 @@ public interface MessageRepository extends ReactiveMongoRepository<Message, Long
     Flux<Message> findByChatRoomId(Long roomId);
 
     Flux<Message> findByChatroomIdAndMessageIdGreaterThanEqual(Long chatroomId, Long lastMessageId);
+
+    @Query("{ 'chatroomId': ?0 }")
+    Flux<Message> findLatestByChatroomId(Long chatroomId, Pageable pageable);
+
 
 }
