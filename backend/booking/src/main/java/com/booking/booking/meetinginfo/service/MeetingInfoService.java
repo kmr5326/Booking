86a118dcc3e9f12962bc6f82dcd5f673f75efbf5
신상원 <dnjsml30@naver.com>
2023-11-15 15:55:11 +0900

    feat: fix meeting time for test

--- backend/booking/src/main/java/com/booking/booking/meetinginfo/service/MeetingInfoService.java
+++ backend/booking/src/main/java/com/booking/booking/meetinginfo/service/MeetingInfoService.java
@@ -19,8 +19,8 @@ public class MeetingInfoService {
 
     public Mono<MeetingInfo> createMeetingInfo(MeetingInfo meetingInfo) {
         log.info("[Booking:MeetingInfo] createMeetingInfo({})", meetingInfo);
-
-        if (meetingInfo.getDate().isBefore(LocalDateTime.now().plusHours(1L))) {
+        // 테스트용으로 바꿔둠
+        if (meetingInfo.getDate().isBefore(LocalDateTime.now().plusMinutes(1L))) {
             return Mono.error(new RuntimeException("시간 다시"));
         }
         return meetingInfoRepository.save(meetingInfo)
