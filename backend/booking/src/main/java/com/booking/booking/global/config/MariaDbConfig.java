b53007c061ad892d0844d8b8cf3f4b2b77c8b441
김한결 <kmr5326@naver.com>
2023-11-15 09:57:10 +0900

    fix: mariadb config

--- backend/booking/src/main/java/com/booking/booking/global/config/MariaDbConfig.java
+++ backend/booking/src/main/java/com/booking/booking/global/config/MariaDbConfig.java
@@ -1,49 +1,49 @@
-package com.booking.booking.global.config;
-
-import io.micrometer.common.lang.NonNullApi;
-import io.r2dbc.pool.ConnectionPool;
-import io.r2dbc.pool.ConnectionPoolConfiguration;
-import io.r2dbc.spi.ConnectionFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.r2dbc.ConnectionFactoryBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
-import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
-
-import java.time.Duration;
-
-@Configuration
-@EnableR2dbcAuditing
-@NonNullApi
-public class MariaDbConfig extends AbstractR2dbcConfiguration {
-    @Value("${maria.url}")
-    private String url;
-    @Value("${maria.user}")
-    private String user;
-    @Value("${maria.password}")
-    private String password;
-
-    @Bean
-    @Override
-    public ConnectionFactory connectionFactory() {
-        ConnectionFactory connectionFactory = ConnectionFactoryBuilder
-                .withUrl(url)
-                .username(user)
-                .password(password)
-                .build();
-
-        ConnectionPoolConfiguration connectionPoolConfiguration
-                = ConnectionPoolConfiguration.builder(connectionFactory)
-                .initialSize(5)
-                .minIdle(2)
-                .maxSize(5)
-                .maxAcquireTime(Duration.ofSeconds(5))
-                .maxCreateConnectionTime(Duration.ofSeconds(5))
-                .maxLifeTime(Duration.ofSeconds(-1))
-                .maxIdleTime(Duration.ofSeconds(-1))
-                .build();
-
-        return new ConnectionPool(connectionPoolConfiguration);
-    }
-}
+//package com.booking.booking.global.config;
+//
+//import io.micrometer.common.lang.NonNullApi;
+//import io.r2dbc.pool.ConnectionPool;
+//import io.r2dbc.pool.ConnectionPoolConfiguration;
+//import io.r2dbc.spi.ConnectionFactory;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.r2dbc.ConnectionFactoryBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
+//import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
+//
+//import java.time.Duration;
+//
+//@Configuration
+//@EnableR2dbcAuditing
+//@NonNullApi
+//public class MariaDbConfig extends AbstractR2dbcConfiguration {
+//    @Value("${maria.url}")
+//    private String url;
+//    @Value("${maria.user}")
+//    private String user;
+//    @Value("${maria.password}")
+//    private String password;
+//
+//    @Bean
+//    @Override
+//    public ConnectionFactory connectionFactory() {
+//        ConnectionFactory connectionFactory = ConnectionFactoryBuilder
+//                .withUrl(url)
+//                .username(user)
+//                .password(password)
+//                .build();
+//
+//        ConnectionPoolConfiguration connectionPoolConfiguration
+//                = ConnectionPoolConfiguration.builder(connectionFactory)
+//                .initialSize(5)
+//                .minIdle(2)
+//                .maxSize(5)
+//                .maxAcquireTime(Duration.ofSeconds(5))
+//                .maxCreateConnectionTime(Duration.ofSeconds(5))
+//                .maxLifeTime(Duration.ofSeconds(-1))
+//                .maxIdleTime(Duration.ofSeconds(-1))
+//                .build();
+//
+//        return new ConnectionPool(connectionPoolConfiguration);
+//    }
+//}
